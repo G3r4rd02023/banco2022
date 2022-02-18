732229592f0f28c4572ac6da554373ee5d4ae2de
Laly Durón <LalyDOsorto@gmail.com>
2022-02-18 10:21:31 -0600

    first commit

---
+++ Banco2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Banco2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
